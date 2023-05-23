4c3c5bfd3c566c0518dc10b5c63cf92ed5f71ff8
lil-doudou <67382584+lil-doudou@users.noreply.github.com>
2023-05-23 06:32:06 +0200

    base

---
+++ wh.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+WEBHOOK=$(cat WEBHOOK.txt)
+
+function curl_webhook() {
+    curl -s \
+      -F "payload_json={\"username\": \"scanner\", \"content\": \"$COMMAND --- $(date)\"}" \
+      -F "file1=@$TEMP_FILE" \
+      $WEBHOOK > /dev/null
+}
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        --share=*)
+            TEMP_FILE=$(echo $1 | awk -F'=' '{print $2}')
+            curl_webhook
+            echo -e "File sended !"
+            exit 0
+        ;;
+        *)
+            COMMAND="$@"
+            TEMP_FILE=$(mktemp --suffix=.txt)
+            RESULT=$($@ > $TEMP_FILE)
+            cat $TEMP_FILE
+            curl_webhook
+            rm $TEMP_FILE
+            break
+        ;;
+    esac
+done
